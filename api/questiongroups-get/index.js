6f20b35fb97a4c2feaa5ecb8078276532500dcdd
Hennyg <henrik.nygaard@live.com>
2026-02-13 11:28:11 +0100

    Update index.js

--- api/questiongroups-get/index.js
+++ api/questiongroups-get/index.js
@@ -1,4 +1,3 @@
-// /api/questiongroups-get/index.js
 const { dvFetch } = require('../_dataverse');
 
 module.exports = async function (context, req) {
@@ -6,21 +5,17 @@ module.exports = async function (context, req) {
     const { id, surveyTypeId, top = 500 } = req.query;
 
     if (id) {
-      // Hent én gruppe
       const r = await dvFetch(
-        `crcc8_lch_questiongroups(${id})?$select=crcc8_lch_questiongroupid,crcc8_lch_name,crcc8_lch_title,crcc8_lch_description,crcc8_lch_sortorder,crcc8_lch_isactive,_crcc8_lch_surveytype_value`
+        `crcc8_lch_questiongroups(${id})?$select=crcc8_lch_questiongroupid,crcc8_lch_name,crcc8_lch_title,crcc8_lch_description,crcc8_lch_sortorder,crcc8_lch_isactive,crcc8_lch_color,_crcc8_lch_surveytype_value`
       );
       const row = await r.json();
       return (context.res = { body: row });
     }
 
-    // Liste grupper (evt. filtreret på surveyTypeId)
-    const filter = surveyTypeId
-      ? `&$filter=_crcc8_lch_surveytype_value eq ${surveyTypeId}`
-      : '';
+    const filter = surveyTypeId ? `&$filter=_crcc8_lch_surveytype_value eq ${surveyTypeId}` : '';
 
     const r = await dvFetch(
-      `crcc8_lch_questiongroups?$select=crcc8_lch_questiongroupid,crcc8_lch_name,crcc8_lch_title,crcc8_lch_description,crcc8_lch_sortorder,crcc8_lch_isactive,_crcc8_lch_surveytype_value${filter}&$orderby=crcc8_lch_sortorder asc&$top=${encodeURIComponent(top)}`
+      `crcc8_lch_questiongroups?$select=crcc8_lch_questiongroupid,crcc8_lch_name,crcc8_lch_title,crcc8_lch_description,crcc8_lch_sortorder,crcc8_lch_isactive,crcc8_lch_color,_crcc8_lch_surveytype_value${filter}&$orderby=crcc8_lch_sortorder asc&$top=${encodeURIComponent(top)}`
     );
     const data = await r.json();
     return (context.res = { body: data });
