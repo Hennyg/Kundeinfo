674715f363f435b83328e074ca37c972cb1fb115
Hennyg <henrik.nygaard@live.com>
2026-02-13 10:32:34 +0100

    Update index.js

--- api/questions-get/index.js
+++ api/questions-get/index.js
@@ -4,12 +4,31 @@ const { dvFetch } = require('../_dataverse');
 module.exports = async function (context, req) {
   try {
     const { id, top = 100 } = req.query;
+
+    const select =
+      [
+        'crcc8_lch_questionid',
+        'crcc8_lch_number',
+        'crcc8_lch_text',
+        'crcc8_lch_explanation',
+        // behold gamle (hvis du stadig bruger dem)
+        'crcc8_lch_group',
+        'crcc8_lch_answertype',
+        'crcc8_lch_isrequired',
+        'crcc8_lch_conditionalon',
+        'crcc8_lch_conditionalvalue',
+        // NEW: lookup til group
+        '_crcc8_lch_questiongroup_value'
+      ].join(',');
+
     if (id) {
-      const r = await dvFetch(`crcc8_lch_questions(${id})?$select=crcc8_lch_questionid,crcc8_lch_number,crcc8_lch_text,crcc8_lch_explanation,crcc8_lch_group,crcc8_lch_answertype,crcc8_lch_isrequired,crcc8_lch_conditionalon,crcc8_lch_conditionalvalue`);
+      const r = await dvFetch(`crcc8_lch_questions(${id})?$select=${select}`);
       const q = await r.json();
       return (context.res = { body: q });
     } else {
-      const r = await dvFetch(`crcc8_lch_questions?$select=crcc8_lch_questionid,crcc8_lch_number,crcc8_lch_text,crcc8_lch_explanation,crcc8_lch_group,crcc8_lch_answertype,crcc8_lch_isrequired,crcc8_lch_conditionalon,crcc8_lch_conditionalvalue&$orderby=crcc8_lch_number asc&$top=${encodeURIComponent(top)}`);
+      const r = await dvFetch(
+        `crcc8_lch_questions?$select=${select}&$orderby=crcc8_lch_number asc&$top=${encodeURIComponent(top)}`
+      );
       const data = await r.json();
       return (context.res = { body: data });
     }
