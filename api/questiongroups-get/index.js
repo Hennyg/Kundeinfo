6fddb46b6531e642bd5ec4173d59d2c903955c95
Hennyg <henrik.nygaard@live.com>
2026-02-16 08:31:48 +0100

    Update index.js

--- api/questiongroups-get/index.js
+++ api/questiongroups-get/index.js
@@ -5,9 +5,13 @@ module.exports = async function (context, req) {
     const { id, surveyTypeId, top = 500 } = req.query;
 
     if (id) {
-      const r = await dvFetch(
-        `crcc8_lch_questiongroups(${id})?$select=crcc8_lch_questiongroupid,crcc8_lch_name,crcc8_lch_title,crcc8_lch_description,crcc8_lch_sortorder,crcc8_lch_isactive,crcc8_lch_color,_crcc8_lch_surveytype_value`
-      );
+const r = await dvFetch(
+  `crcc8_lch_questiongroups?` +
+  `$select=crcc8_lch_questiongroupid,crcc8_lch_name,crcc8_lch_title,crcc8_lch_description,crcc8_lch_sortorder,crcc8_lch_isactive,crcc8_lch_color,_crcc8_lch_surveytype_value` +
+  `&$expand=crcc8_lch_surveytype($select=crcc8_lch_type)` +
+  `${filter}` +
+  `&$orderby=crcc8_lch_sortorder asc&$top=${encodeURIComponent(top)}`
+);
       const row = await r.json();
       return (context.res = { body: row });
     }
