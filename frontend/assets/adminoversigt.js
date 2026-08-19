6c4383e3bf0ead6a978d9f7d6cc71d2e6fab615e
Hennyg <henrik.nygaard@live.com>
2026-08-19 10:36:00 +0200

    Add modified date display to admin overview

--- frontend/assets/adminoversigt.js
+++ frontend/assets/adminoversigt.js
@@ -66,6 +66,7 @@ function rowHtml(row) {
       <td>${statusPillHtml(row)}</td>
       <td>${fmtDateTime(expiresAt)}${expiredTag}</td>
       <td>${fmtDateTime(row.createdon)}</td>
+      <td>${fmtDateTime(row.modifiedon)}</td>
       <td>
         <a class="tag" href="${seSkemaLink}" target="_blank" rel="noopener">Se skema</a>
         <a class="tag" href="${prefillLink}">Prefill</a>
